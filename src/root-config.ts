8cdc96e48efc28e1a40e1149566e2e3838aa0675
Brandon Istenes <bistenes@gmail.com>
2019-10-09 14:39:56 -0500

    Update to esm-module-config

--- src/root-config.ts
+++ src/root-config.ts
@@ -1,7 +1,7 @@
 import { start } from "single-spa";
 // import { registerAllCoreApplications } from "@openmrs/esm-root-config";
 import { registerAllCoreApplications } from "@openmrs/esm-root-config/dist/openmrs-esm-root-config.lib.js";
-import { provide } from "@openmrs/esm-config";
+import { provide } from "@openmrs/esm-module-config";
 import config from "./config.json";
 
 provide(config);
