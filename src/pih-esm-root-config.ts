f7360e76b6386c502de00c2f00747f937adff68d
Brandon Istenes <bistenes@gmail.com>
2020-02-12 15:41:44 -0600

    update single-spa and esm-module-config

--- src/pih-esm-root-config.ts
+++ src/pih-esm-root-config.ts
@@ -1,6 +1,5 @@
 import { start } from "single-spa";
-// import { registerAllCoreApplications } from "@openmrs/esm-root-config";
-import { registerAllCoreApplications } from "@openmrs/esm-root-config/dist/openmrs-esm-root-config.lib.js";
+import { registerAllCoreApplications } from "@openmrs/esm-root-config";
 import { provide } from "@openmrs/esm-module-config";
 import config from "./config.json";
 
