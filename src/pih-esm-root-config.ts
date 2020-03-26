0fb5a64e29519858aed8979ee93a8764ac4d9a62
Brandon Istenes <bistenes@gmail.com>
2020-03-26 12:45:09 -0700

    Add referrals queue

--- src/pih-esm-root-config.ts
+++ src/pih-esm-root-config.ts
@@ -1,9 +1,14 @@
-import { start } from "single-spa";
+import { start, registerApplication } from "single-spa";
 import { registerAllCoreApplications } from "@openmrs/esm-root-config";
 import { provide } from "@openmrs/esm-module-config";
 import config from "./config.json";
 
 provide(config);
 registerAllCoreApplications();
+registerApplication(
+  "@pih/esm-referrals-queue",
+  () => System.import("@pih/esm-referrals-queue"),
+  (location) => location.pathname.includes("/referrals-queue")
+);
 
 start();
